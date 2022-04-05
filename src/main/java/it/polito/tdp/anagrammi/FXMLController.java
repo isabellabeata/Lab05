b46e5b38d1d14a17fbec7beecc62cc7a15fb199a
isabe <isabe@172.22.173.28>
2022-04-05 14:19:16 +0200

    fine es

--- src/main/java/it/polito/tdp/anagrammi/FXMLController.java
+++ src/main/java/it/polito/tdp/anagrammi/FXMLController.java
@@ -1,6 +1,77 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
 package it.polito.tdp.anagrammi;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.anagrammi.model.Anagramma;
+import it.polito.tdp.anagrammi.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private Model model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnCalcolaAnagrammi"
+    private Button btnCalcolaAnagrammi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAnagrammiCorretti"
+    private TextArea txtAnagrammiCorretti; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAnagrammiErrati"
+    private TextArea txtAnagrammiErrati; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtParolaInserita"
+    private TextField txtParolaInserita; // Value injected by FXMLLoader
+
+    @FXML
+    void handleCalcolaAnagram(ActionEvent event) {
+    	
+    	String parola= this.txtParolaInserita.getText();
+    	model.anagramma(parola);
+    	for(Anagramma ai: model.getListaAnagrammi()) {
+    		if(ai.isCorrect()) {
+    			this.txtAnagrammiCorretti.appendText(ai.getAnagramma()+"\n");
+    		}else
+    			this.txtAnagrammiErrati.appendText(ai.getAnagramma()+"\n");
+    	}
+
+    }
+
+    @FXML
+    void handleReset(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btnCalcolaAnagrammi != null : "fx:id=\"btnCalcolaAnagrammi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtAnagrammiCorretti != null : "fx:id=\"txtAnagrammiCorretti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtAnagrammiErrati != null : "fx:id=\"txtAnagrammiErrati\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtParolaInserita != null : "fx:id=\"txtParolaInserita\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model=model;
+		
+	}
 
 }
